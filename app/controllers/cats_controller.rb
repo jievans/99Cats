df5a31aedd2475d0f7546b8ff515e8f2ffb72671
App Academy Student <student@appacademy.io>
2013-08-08 12:01:52 -0700

    Finished step 1

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -2,19 +2,25 @@ class CatsController < ApplicationController
 
   def index
     @all_cats = Cat.all
+    render :index
   end
 
   def show
     @cat = Cat.find(params[:id])
+    render :show
   end
 
   def new
+    render :new
   end
 
   def create
+    cat = Cat.create!(params[:cat])
+    redirect_to cat_url cat
   end
 
   def edit
+    @cat = Cat.find(params[:id])
   end
 
 end
