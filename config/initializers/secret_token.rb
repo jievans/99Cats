440f202a7f839c494969ee87ee02272ce0ea2aa4
App Academy Student <student@appacademy.io>
2013-08-08 10:52:27 -0700

    First commit; initialized rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NinetyNineCats::Application.config.secret_token = '3ab325a5a277a5d5fd90b7a1cda6e64d64f404d35692f499207d7591e8f839e14ffbc9d4f4e233b6f2425b0425546ea05ccd4b8ee5fc01ab4327691e700d3517'
